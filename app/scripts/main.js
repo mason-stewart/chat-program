c22f55ac932c27fbcb5294e00e778ff456a1e924
Pickra <picker10024@yahoo.com>
2013-09-18 15:08:43 -0400

    added parse keys and source files

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+Parse.initialize("hM7WAdklsjmnzqt381MfrXpcwlXy68a6VAQ4jdr9", "qFcvl9ZRt48yFxQACHV53qouuVXVViVvMzY4FDug");
+
+// var TestObject = Parse.Object.extend("TestObject");
+// var testObject = new TestObject();
+// testObject.save({foo: "bar"}, {
+//   success: function(object) {
+//     alert("yay! it worked");
+//   }
+// });
