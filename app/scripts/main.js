c61e86507cdb9ad1547a89ebfc779ac818c4d6f9
Pickra <picker10024@yahoo.com>
2013-09-19 14:49:39 -0400

    created msg obj, collection, and constructor, save message function, and refresh capabilities function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,3 +12,48 @@ var chatMessage = _.template($("#chat-message"));
 chatMessage({
 	message: message
 })
+
+var Message = Parse.Object.extend("Message");
+
+var messageCollection = Parse.Collection.extend({		
+	model: Message						
+});
+
+var message = new Message();	
+
+					// function sendItToParse(){
+					// 	$('.form-control').append(message);
+					// }
+
+
+$('.submit-btn').click(function(){ 
+	
+
+});
+
+// function to save message to parse
+
+function saveMessage(messageArg){
+	message.set('message-content', $('.form-control').val());
+	message.set('message-user', $('#username').val());
+	
+	message.save(null, {						
+
+	success: function(msg){
+		console.log('message saved, YEAH!!!!!!!!!!!!!!!!!')
+	}, 
+	error: function(msg, error){
+		console.log('message not saved, BOOOO!!!!!!!!!!!!!!!!!')
+	}
+	}		
+}
+
+function refreshMessages(){
+	messageCollection.fetch({
+		success: function(collection){
+			collection.each(function(msg){
+				addToChatWindow(msg)
+			})
+		}
+	})
+}
