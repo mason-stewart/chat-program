c5f8d4facc31b2612f727dee2a54d7163dbf1e88
Jake Hendley <jake.hendley25@gmail.com>
2013-09-19 16:27:31 -0400

    added refreshRoutes() and addToChatWindow(), need to debug

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,59 +1,53 @@
-Parse.initialize("hM7WAdklsjmnzqt381MfrXpcwlXy68a6VAQ4jdr9", "qFcvl9ZRt48yFxQACHV53qouuVXVViVvMzY4FDug");
-
-// var TestObject = Parse.Object.extend("TestObject");
-// var testObject = new TestObject();
-// testObject.save({foo: "bar"}, {
-//   success: function(object) {
-//     alert("yay! it worked");
-//   }
-// });
-
-var chatMessage = _.template($("#chat-message"));
-chatMessage({
-	message: message
+$(document).ready(function(){
+	refreshMessages()
+	setInterval(refreshMessages, 3000);
+	$("#submit-btn").click(function(){saveMessage()})
 })
 
-var Message = Parse.Object.extend("Message");
 
-var messageCollection = Parse.Collection.extend({		
+
+var Message = Parse.Object.extend("Message");
+var message = new Message();	
+var MessageCollection = Parse.Collection.extend({		
 	model: Message						
 });
+var messages = new MessageCollection();
 
-var message = new Message();	
-
-					// function sendItToParse(){
-					// 	$('.form-control').append(message);
-					// }
-
-
-$('.submit-btn').click(function(){ 
-	
-
-});
 
 // function to save message to parse
 
 function saveMessage(messageArg){
-	message.set('message-content', $('.form-control').val());
-	message.set('message-user', $('#username').val());
+	message.set('username', $('#username').val());
+	message.set('content', $('.form-control').val());
 	
 	message.save(null, {						
 
-	success: function(msg){
-		console.log('message saved, YEAH!!!!!!!!!!!!!!!!!')
-	}, 
-	error: function(msg, error){
-		console.log('message not saved, BOOOO!!!!!!!!!!!!!!!!!')
-	}
-	}		
+		success: function(msg){
+			console.log('message saved, YEAH!!!!!!!!!!!!!!!!!')
+			refreshMessages()
+		}, 
+		error: function(msg, error){
+			console.log('message not saved, BOOOO!!!!!!!!!!!!!!!!!')
+		}
+	})	
 }
 
 function refreshMessages(){
-	messageCollection.fetch({
+	messages.fetch({
 		success: function(collection){
 			collection.each(function(msg){
 				addToChatWindow(msg)
 			})
 		}
 	})
+}
+
+
+function addToChatWindow(message){
+
+	var chatMessage = _.template($("#chat-message").text());
+	chatMessage({
+		message: message
+	})
+	$(".chat-window").append(chatMessage)
 }
