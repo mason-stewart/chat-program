cd389e68588f45d72330986e9c2cf6c61036878e
Pickra <picker10024@yahoo.com>
2013-09-20 23:17:02 -0400

    added checkForNewChat function. debugged(ish) getMessages function. called timeoutRefresh() in doc ready. moved username input. went over jake's additions (as of sept 20, 2013); tried to make sense of them and add them to my files. added obnoxious text to various places.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,8 @@
-Parse.initialize("hM7WAdklsjmnzqt381MfrXpcwlXy68a6VAQ4jdr9", "qFcvl9ZRt48yFxQACHV53qouuVXVViVvMzY4FDug");
 
-// var TestObject = Parse.Object.extend("TestObject");
-// var testObject = new TestObject();
-// testObject.save({foo: "bar"}, {
-//   success: function(object) {
-//     alert("yay! it worked");
-//   }
-// });
-
-var chatMessage = _.template($("#chat-message"));
-chatMessage({
-	message: message
+$(document).ready(function(){
+	getMessages()
+	$("#submit-btn").click(function(){saveMessage()})
+	timeoutRefresh()
 })
 
 var Message = Parse.Object.extend("Message");
@@ -19,41 +11,57 @@ var messageCollection = Parse.Collection.extend({
 	model: Message						
 });
 
-var message = new Message();	
-
-					// function sendItToParse(){
-					// 	$('.form-control').append(message);
-					// }
-
-
-$('.submit-btn').click(function(){ 
-	
-
-});
+var messages = new messageCollection();	
 
 // function to save message to parse
 
-function saveMessage(messageArg){
-	message.set('message-content', $('.form-control').val());
-	message.set('message-user', $('#username').val());
-	
-	message.save(null, {						
+function saveMessage(){
+	var message = new Message();
+	message.set('username', $('#username').val());
+	message.set('content', $('.form-control').val());
+	$('.form-control').val(''); /*  set the users input values, line 19 + 20, onto parse server and then clear the input fields. nice jake */
 
-	success: function(msg){
-		console.log('message saved, YEAH!!!!!!!!!!!!!!!!!')
-	}, 
-	error: function(msg, error){
-		console.log('message not saved, BOOOO!!!!!!!!!!!!!!!!!')
-	}
-	}		
+	message.save(message, {						
+
+		success: function(message){
+			console.log('message saved, YEAH!!!!!!!!!!!!!!!!!')
+		}, 
+		error: function(message, error){
+			console.log('message not saved, BOOOO!!!!!!!!!!!!!!!!!')
+		}
+	})		
 }
 
-function refreshMessages(){
-	messageCollection.fetch({
+function getMessages(){
+	messages.fetch({
 		success: function(collection){
+			$('.chat-window').html('')  /*!!! this goes here because parse takes a half a second to refresh, 2 lines up, then clears .chat-window*/
 			collection.each(function(msg){
 				addToChatWindow(msg)
 			})
 		}
 	})
 }
+
+function addToChatWindow(msg){
+	message = msg;
+	var chatMessage = _.template($("#chat-message").text());
+	chatMessage({
+		message: message
+	})
+	$(".chat-window").append(chatMessage)
+}
+
+function timeoutRefresh(){
+	setInterval(checkForNewChat, 3000);
+}
+
+function formatTime(timestamp) {
+	var time = moment(timestamp).format("hh:mm");
+	return time;
+}
+
+function checkForNewChat(){
+	getMessages();				/*!!! as opposed to putting --$('.chat-window').html('')-- here, because it clears the .chat-window before parse updates/refreshes/fetches so you see the half a second it takes parse to refresh happening. clear screen, wait half a second, then refresh*/
+}
+
